634d76d345323ef896a7c7a3775fe3972c1bff62
kurth4cker <kurth4cker@gmail.com>
2025-01-10 18:13:24 +0300

    Initialize repository.

---
+++ sample.go
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package sample
+
+// Greeting string which greets world
+var GreetingWorld string = "hello world"
