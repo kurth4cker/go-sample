55c225ec3a54142f2023ab4c737bd5abf1ae8087
kurth4cker <kurth4cker@gmail.com>
2025-01-13 17:35:00 +0300

    greet.go: add package documentation

--- greet.go
+++ greet.go
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
 
+// Sample Go repository for trying out any module related tech
 package sample
 
 import "fmt"
