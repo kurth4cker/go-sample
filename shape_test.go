0801d14dc999bb0c1b460a3b617b90c0a13e3af2
kurth4cker <kurth4cker@gmail.com>
2025-02-12 00:41:05 +0300

    reorganize test driven development sources

---
+++ shape_test.go
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2025 Cristopher James
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package sample
+
+import "testing"
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Height: 12, Width: 6}, want: 72},
+		{name: "Rectangle", shape: Rectangle{Height: 10, Width: 10}, want: 100},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36},
+	}
+
+	for _, tc := range areaTests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.shape.Area()
+			if got != tc.want {
+				t.Errorf("%#v got %g, want %g",
+					tc.shape, got, tc.want)
+			}
+		})
+	}
+}
---
+++ wallet.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2025 Cristopher James
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package sample
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
