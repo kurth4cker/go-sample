510d0eb42a42bcbb3331c72315b9935772e3b370
kurth4cker <kurth4cker@gmail.com>
2025-02-18 02:29:39 +0300

    add doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+// Sample Go repository for trying out any module related tech
+package sample
