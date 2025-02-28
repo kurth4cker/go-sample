6e1b66e2ea23bd0abfa7c3f01384425b274c97ae
kurth4cker <kurth4cker@gmail.com>
2025-03-01 02:40:24 +0300

    list: add example tests

---
+++ list/list_example_test.go
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package list_test
+
+import (
+	"fmt"
+	"slices"
+
+	"codeberg.org/kurth4cker/go-sample/list"
+)
+
+func Example() {
+	lst := list.List[int]{}
+	for i := range 4 {
+		lst.Push(i)
+	}
+
+	numbers := slices.Collect(lst.All())
+	fmt.Println(numbers)
+	// Output: [3 2 1 0]
+}
