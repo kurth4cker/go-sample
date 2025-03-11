760cc1965b600aa32db3c941ae43520dc17c457f
kurth4cker <kurth4cker@gmail.com>
2025-03-12 01:21:38 +0300

    stack: remove duplicate tests

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -11,38 +11,20 @@ import (
 )
 
 func TestArray(t *testing.T) {
-	t.Run("Int", func(t *testing.T) {
-		ints := new(stack.Array[int])
-		assert.True(t, ints.IsEmpty())
+	ints := new(stack.Array[int])
+	assert.True(t, ints.IsEmpty())
 
-		ints.Push(123)
-		assert.False(t, ints.IsEmpty())
+	ints.Push(123)
+	assert.False(t, ints.IsEmpty())
 
-		ints.Push(456)
-		value, _ := ints.Pop()
-		assert.Equal(t, value, 456)
+	ints.Push(456)
+	value, _ := ints.Pop()
+	assert.Equal(t, value, 456)
 
-		value, _ = ints.Pop()
-		assert.Equal(t, value, 123)
-		assert.True(t, ints.IsEmpty())
+	value, _ = ints.Pop()
+	assert.Equal(t, value, 123)
+	assert.True(t, ints.IsEmpty())
 
-		_, ok := ints.Pop()
-		assert.False(t, ok)
-	})
-
-	t.Run("String", func(t *testing.T) {
-		strings := new(stack.Array[string])
-		assert.True(t, strings.IsEmpty())
-
-		strings.Push("hello")
-		assert.False(t, strings.IsEmpty())
-
-		strings.Push("bye")
-		value, _ := strings.Pop()
-		assert.Equal(t, value, "bye")
-
-		value, _ = strings.Pop()
-		assert.Equal(t, value, "hello")
-		assert.True(t, strings.IsEmpty())
-	})
+	_, ok := ints.Pop()
+	assert.False(t, ok)
 }
