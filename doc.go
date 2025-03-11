6b40b077122199eede6bce667df603af6f8ca0a0
kurth4cker <kurth4cker@gmail.com>
2025-03-12 01:25:07 +0300

    sample: remove doc.go

--- doc.go
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MPL-2.0
-// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
-
-// Sample Go repository for trying out any module related tech
-package sample
