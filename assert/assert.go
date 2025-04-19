fbaa6529971de235c16ad494a0c426f63b3d585e
kurth4cker <kurth4cker@gmail.com>
2025-04-19 12:53:10 +0300

    assert: rename variable t to tb

--- assert/assert.go
+++ assert/assert.go
@@ -9,32 +9,32 @@ import "testing"
 //
 // If they are not equal call tb.Errorf.
 func Equal[T comparable](tb testing.TB, got, want T) {
-	t.Helper()
+	tb.Helper()
 	if got != want {
-		t.Errorf("got %+v, want %+v", got, want)
+		tb.Errorf("got %+v, want %+v", got, want)
 	}
 }
 
 // Same as [Equal] but test with given function.
-func EqualFunc[T any](t testing.TB, got, want T, eq func(T, T) bool) {
-	t.Helper()
+func EqualFunc[T any](tb testing.TB, got, want T, eq func(T, T) bool) {
+	tb.Helper()
 	if !eq(got, want) {
-		t.Errorf("got %+v, want %+v", got, want)
+		tb.Errorf("got %+v, want %+v", got, want)
 	}
 }
 
 // If given value is not true, fail.
-func True(t testing.TB, got bool) {
-	t.Helper()
+func True(tb testing.TB, got bool) {
+	tb.Helper()
 	if !got {
-		t.Errorf("got %v, want true", got)
+		tb.Errorf("got %v, want true", got)
 	}
 }
 
 // Fail if value is false.
-func False(t testing.TB, got bool) {
-	t.Helper()
+func False(tb testing.TB, got bool) {
+	tb.Helper()
 	if got {
-		t.Errorf("got %v, want false", got)
+		tb.Errorf("got %v, want false", got)
 	}
 }
